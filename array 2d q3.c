038e80b0f268d0e0f4fb50da2d72b09e232253c7
Soumava-221B <77767745+Soumava-221B@users.noreply.github.com>
2021-10-16 13:19:23 +0530

    Add Array C programing files

---
+++ array 2d q3.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,sum=0,a[2][2],b[2][2],c[2][2];
+    printf("Enter 4 numbers\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Enter 4 no.\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            c[i][j]=a[i][j]+b[i][j];
+        }
+    }
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d\t",c[i][j]);
+        }
+        printf("\n");
+    }
+}
